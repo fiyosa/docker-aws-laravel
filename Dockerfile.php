73e047391273c95d2dc796edc292b4e035ed72ab
fiyosa <fiyosa97@gmail.com>
2023-04-16 18:53:24 +0700

    fix bug

--- Dockerfile.php
+++ Dockerfile.php
@@ -1,26 +1,34 @@
 FROM php:8.1.18-fpm AS php-builder
 
-RUN apt-get update && apt-get install -y \
-    git \
-    zip \
-    unzip
-
-# Set working directory
-WORKDIR /app
-
-# Copy file composer.json
-COPY --from=composer-builder /app ./
-
 # Install dependensi PHP dan PHP-FPM
 RUN apt-get update && apt-get install -y \
     libzip-dev \
     unzip \
-    && docker-php-ext-install zip
+    libpq-dev
 
 RUN docker-php-ext-install pdo pdo_pgsql
+
+# Set working directory
+WORKDIR /var/www/html
+
+# Copy file composer.json dan composer.lock ke dalam container
+COPY composer.json ./
+
+# Install dependencies menggunakan Composer
+RUN composer install --no-scripts --no-autoloader
+
+# Copy seluruh isi proyek Laravel ke dalam container
+COPY . .
+
+# Autoload Composer
+RUN composer dump-autoload --optimize
 
 RUN php artisan route:clear
 
 RUN php artisan cache:clear
 
 RUN php artisan optimize:clear
+
+EXPOSE 8081
+
+CMD ["php-fpm"]
